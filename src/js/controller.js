c196fdc32d036259571f8b2380fdda59b5617d64
Carlos Fernando Alcala Padilla <carlos_alcala8777@jabil.com>
2026-09-07 19:37:07 -0600

    Se carga la receta dinamicamente desde el hash de la URL con los eventos hashchange y load

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,10 +28,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     renderSpinner(recipeContainer);
 
     const resp = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await resp.json();
     console.log(resp);
@@ -148,4 +153,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
