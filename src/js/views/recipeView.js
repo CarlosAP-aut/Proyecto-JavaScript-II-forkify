44a135d283b28d25478ff5d65fdc2e48d0182115
Carlos Fernando Alcala Padilla <carlos_alcala8777@jabil.com>
2026-09-07 19:51:00 -0600

    Se corrige el calculo de fracciones migrando a la libreria fraction.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,4 @@
-import { Fraction } from 'fractional';
+import { Fraction } from 'fraction.js';
 import icons from 'url:../../img/icons.svg';
 
 class RecipeView {
@@ -93,7 +93,7 @@ class RecipeView {
                   <use href="${icons}#icon-check"></use>
                 </svg>
                 <div class="recipe__quantity">${
-                  ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                  ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ''
                 }</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
