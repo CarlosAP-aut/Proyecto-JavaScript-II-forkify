1f1608fd266f05601faebca73a08bed436ae5dc6
Carlos Fernando Alcala Padilla <carlos_alcala8777@jabil.com>
2026-09-07 20:10:55 -0600

    Se conecta el formulario de busqueda usando el patron editor-suscriptor

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,6 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -8,7 +9,6 @@ import recipeView from './views/recipeView.js';
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
 
@@ -22,7 +22,19 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResults(query);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -24,7 +24,6 @@ export const loadRecipe = async function (id) {
       cookTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(state.recipe);
   } catch (err) {
     console.log(`${err} 💥💥💥💥`);
     throw err;
