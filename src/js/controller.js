c3747ed0b848f91250c05993c82a5b2b0e0390d4
Carlos Fernando Alcala Padilla <carlos_alcala8777@jabil.com>
2026-09-07 20:12:14 -0600

    Se despliegan los resultados de la busqueda con la clase ResultsView

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -24,10 +25,14 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     const query = searchView.getQuery();
     if (!query) return;
 
     await model.loadSearchResults(query);
+
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
