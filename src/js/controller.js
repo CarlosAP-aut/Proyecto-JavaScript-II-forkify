fc0763a0d65ac8596b760335efdd73d661d3e2d1
Carlos Fernando Alcala Padilla <carlos_alcala8777@jabil.com>
2026-09-11 12:47:54 -0600

    Se agrega paginationView con botones para navegar entre paginas

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -33,6 +34,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     resultsView.render(model.getSearchResultsPage());
+
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
@@ -41,5 +44,9 @@ const controlSearchResults = async function () {
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(function (page) {
+    resultsView.render(model.getSearchResultsPage(page));
+    paginationView.render(model.state.search);
+  });
 };
 init();
